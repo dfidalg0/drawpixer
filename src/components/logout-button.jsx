b866dcde1c7e1c0b9f08f0305696f9de3f275906
Diego <diego-fidalgo@hotmail.com>
2020-11-08 14:50:47 -0300

    Fix: Ausência de ID do Cliente Google

--- src/components/logout-button.jsx
+++ src/components/logout-button.jsx
@@ -17,7 +17,8 @@ function LogoutButton({ logout }) {
     const classes = useStyles();
     const [pending, setPending] = useState(false);
     const { signOut } = useGoogleLogout({
-        onLogoutSuccess: logout
+        onLogoutSuccess: logout,
+        clientId: "700716339246-aau8p35vfa84d5lgrf20g6nm196db0aa.apps.googleusercontent.com"
     });
 
     // Impedindo que a função seja chamada mais de uma vez dentro de 3 segundos
