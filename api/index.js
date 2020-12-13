ca5227f1bb69f5c29a9e7f9e485871a6e2655cae
Diego Fidalgo <diego-fidalgo@hotmail.com>
2020-12-13 16:43:18 -0300

    Headers contra Clickjacking

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
-import cors from 'cors';
 
 // Rotas
 import authRoutes from '../lib/routes/auth';
@@ -11,10 +10,11 @@ const app = express();
 
 app.set('trust proxy', 1);
 
-app.use(cors({
-    origin: /^https:\/\/(www\.)?drawpixer(-git-.+\.diego-fidalgo)?\.vercel\.app$/
+app.use(helmet({
+    frameguard: {
+        action: 'DENY'
+    }
 }));
-app.use(helmet());
 
 app.use(express.json());
 app.use(cookieParser());
