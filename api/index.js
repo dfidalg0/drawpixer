1e5fea94af9f06b32e608385e0bee6d4fc1dc702
Diego Fidalgo <diego-fidalgo@hotmail.com>
2020-12-13 15:23:39 -0300

    Adicionados cors e helmet

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import cors from 'cors';
 
 // Rotas
 import authRoutes from '../lib/routes/auth';
@@ -8,6 +10,11 @@ import drawRoutes from '../lib/routes/drawings';
 const app = express();
 
 app.set('trust proxy', 1);
+
+app.use(cors({
+    origin: /^https:\/\/(www\.)?drawpixer(-git-.+\.diego-fidalgo)?\.vercel\.app$/
+}));
+app.use(helmet());
 
 app.use(express.json());
 app.use(cookieParser());
